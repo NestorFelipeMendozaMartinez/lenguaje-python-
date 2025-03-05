90ec2e452850300e882f5ddcbddd779659f986dd
NestorFelipeMendozaMartinez <mendozamartineznestorfelipe48@gmail.com>
2025-03-05 12:18:33 -0500

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -1,2 +1,2 @@
 print(' \n hola mundo')
-print('\n Servicio Nacional de Aprendizaje SENA \n Analisis y desarrollo de software \n ficha 63066478 \n Esteban Garzon Amaya \n')
+print('\n Servicio Nacional de Aprendizaje SENA \n Analisis y desarrollo de software \n ficha 3066478 \n Nestorf Felipe Mendoza Martinez \n')
